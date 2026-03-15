e0d91e577e964740ff3d2b69298a1e660c0c682b
Robson Ferreira <robson.s.ferreira1989@gmail.com>
2026-03-15 16:04:23 -0300

    Costantes e Variavéis

---
+++ var_const.py
@@ -0,0 +1,16 @@
+age = 28
+name = "Robson"
+print(f"Meu nome é {name} e tenho {age} anos.")
+
+DEBUG = True
+STATES = ["SP", "RJ", "MG", "ES"]
+AMOUNT = 100.50
+
+nome = "Maria"
+idade = 30
+print(f"Meu nome é {nome} e tenho {idade} anos.")
+
+limite_saque_diario = 1000.00
+
+BRAZILIAN_STATES = ["SP", "RJ", "MG", "ES"]
+print
