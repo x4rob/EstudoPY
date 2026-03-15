a1ada7bdc0d3a94b4ec1e4ea199a796a02256d74
Robson Ferreira <robson.s.ferreira1989@gmail.com>
2026-03-15 16:23:39 -0300

    convertendo tipos

---
+++ convertendo_tipos.py
@@ -0,0 +1,8 @@
+print(int(1.5))  # Converte float para inteiro
+print(int("123"))  # Converte string para inteiro
+print(float("10.10")) 
+print(float(100))
+
+valor = 10
+valor_str = str(valor)  # Converte inteiro para string
+print(type(valor_str))  # Verifica o tipo da variável valor_str
