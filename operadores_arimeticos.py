eb8450c7ac340392050ad8a683bff6154539fce7
Robson Ferreira <robson.s.ferreira1989@gmail.com>
2026-03-16 17:26:09 -0300

    convertendo e operadores

---
+++ operadores_arimeticos.py
@@ -0,0 +1,17 @@
+produto_1 = 10
+produto_2 = 20
+print(produto_1 + produto_2) #Soma
+# lembrando quue as operaçãos são feitas da esquerda para a direita, ou seja, primeiro é feita a soma e depois a multiplicação
+print(produto_1 - produto_2)
+print(produto_1 * produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2) #Divisão inteira
+print(produto_1 % produto_2) #Resto da divisão
+print(produto_1 ** produto_2) #Potenciação
+
+
+x = (10 + 5 ) * 2
+y = 10 + 5 * 2 
+
+print(x)
+print(y)
