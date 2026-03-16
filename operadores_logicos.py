069090602a4b1e9c084303abb7001d4465d01065
Robson Ferreira <robson.s.ferreira1989@gmail.com>
2026-03-16 17:52:30 -0300

    operadores lógicos

---
+++ operadores_logicos.py
@@ -0,0 +1,7 @@
+saldo = 1000
+saque = 200
+limite = 100
+# Operadores lógicos
+print(saldo >= saque and limite >= saque)
+print(saldo >= saque or limite >= saque)
+print(not (saldo >= saque and limite >= saque))
